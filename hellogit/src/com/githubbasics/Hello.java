6b1c29616a950b40fa41ec8965391fd95ce9c4c2
muralibestha <muralibestha@192.168.1.4>
2018-09-12 12:56:36 +0530

    updated greeting message

--- hellogit/src/com/githubbasics/Hello.java
+++ hellogit/src/com/githubbasics/Hello.java
@@ -3,7 +3,7 @@ package com.githubbasics;
 public class Hello {
 
 	public static void main(String[] args) {
-		// greeting message
+		// updated greeting message
 		System.out.println("Hello git");
 
 	}
